835ab5bf466e91b35013135f75254bbdd581cb06
geroneia <kozetta2004@yandex.ru>
2020-01-26 20:57:44 +0300

    Меняет некоторые константы на переменные, упрощает код из предыдущего задания

--- js/stat.js
+++ js/stat.js
@@ -10,10 +10,12 @@ var TEXT_WIDTH = 50;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var LINE_HEIGHT = 20;
-var LEFT_MARGIN = GAP * 4;
-var TOP_MARGIN = GAP * 2;
-var DISTANCE_BETWEEN_COLUMNS = GAP * 5;
-var MESSAGE = 'Ура вы победили! \nСписок результатов:';
+var leftMargin = GAP * 4;
+var topMargin = GAP * 2;
+var distanceBetweenColumns = GAP * 5;
+var message = 'Ура вы победили! \nСписок результатов:';
+var histogramX = CLOUD_X + leftMargin;
+var histogtamBottom = CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -32,7 +34,6 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-
 // Выбирается случайный синий
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -47,7 +48,7 @@ var renderHistogram = function (ctx, names, times) {
 
   for (var j = 0; j < names.length; j++) {
     ctx.fillStyle = 'black';
-    ctx.fillText(names[j], CLOUD_X + LEFT_MARGIN + (TEXT_WIDTH + DISTANCE_BETWEEN_COLUMNS) * j, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP);
+    ctx.fillText(names[j], histogramX + (TEXT_WIDTH + distanceBetweenColumns) * j, histogtamBottom);
 
     // Находит текущего игрока и отечает красным
     if (names[j] === 'Вы') {
@@ -56,28 +57,26 @@ var renderHistogram = function (ctx, names, times) {
       ctx.fillStyle = getRandomColor();
     }
     // Рисует столбики диаграммы
-    ctx.fillRect(CLOUD_X + LEFT_MARGIN + (TEXT_WIDTH + DISTANCE_BETWEEN_COLUMNS) * j, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP - GAP - (BAR_HEIGHT * times[j]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[j]) / maxTime);
+    ctx.fillRect(histogramX + (TEXT_WIDTH + distanceBetweenColumns) * j, histogtamBottom - GAP - (BAR_HEIGHT * times[j]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[j]) / maxTime);
 
     // Добавляет статистику в цифрах
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[j]), CLOUD_X + LEFT_MARGIN + (TEXT_WIDTH + DISTANCE_BETWEEN_COLUMNS) * j, CLOUD_Y + CLOUD_HEIGHT - GAP - FONT_GAP - GAP - (BAR_HEIGHT * times[j]) / maxTime - FONT_GAP);
+    ctx.fillText(Math.round(times[j]), histogramX + (TEXT_WIDTH + distanceBetweenColumns) * j, histogtamBottom - GAP - (BAR_HEIGHT * times[j]) / maxTime - FONT_GAP);
   }
 };
 
 window.renderStatistics = function (ctx, names, times) {
-
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
-  // Сообщение о победе разбивается на строки в месте переноса (тут нужна функция, но я не знаю, как правильно ее написать)
+  // Сообщение о победе разбивается на строки в месте переноса
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  var lines = MESSAGE.split('\n');
+  var lines = message.split('\n');
   for (var i = 0; i < lines.length; i++) {
-    ctx.fillText(lines[i], CLOUD_X + LEFT_MARGIN, CLOUD_Y + TOP_MARGIN + (i * LINE_HEIGHT));
+    ctx.fillText(lines[i], CLOUD_X + leftMargin, CLOUD_Y + topMargin + (i * LINE_HEIGHT));
   }
 
   renderHistogram(ctx, names, times);
-
 };
