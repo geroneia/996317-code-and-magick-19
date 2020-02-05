40b7e2b51c1b84cc489e6cb51f3736c88b29a3aa
geroneia <kozetta2004@yandex.ru>
2020-02-06 01:03:04 +0300

    Добавляет недостающую точку с запятой

--- js/dnd.js
+++ js/dnd.js
@@ -41,7 +41,7 @@
       if (dragged) {
         var onClickPreventDefault = function (clickEvt) {
           clickEvt.preventDefault();
-          dialogHandler.removeEventListener('click', onClickPreventDefault)
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
         };
         dialogHandler.addEventListener('click', onClickPreventDefault);
       }
